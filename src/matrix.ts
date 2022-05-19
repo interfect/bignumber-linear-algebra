1da23235743fa6a3873dcdc31dc0b0c6e4559638
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-05-19 22:15:32 +0200

    code split

---
+++ src/matrix.ts
@@ -0,0 +1,118 @@
+import Vec, { VecBasicValues } from "./vector";
+import BigNumber from "bignumber.js";
+
+type MatrixBasicValues = Vec[] | VecBasicValues[];
+
+export default class Matrix {
+  _matrix: Vec[];
+
+  constructor(values?: Vec | Matrix | MatrixBasicValues) {
+    // empty matrix
+    if (!values) { 
+      this._matrix = [];
+    }
+    // values as matrix
+    else if (values instanceof Matrix) {
+      this._matrix = [...values._matrix];
+    }
+    else if (values instanceof Vec) {
+      this._matrix = [values.copy()];
+    }
+    // vector values
+    else if (values.length > 0 && values[0] instanceof Vec) {
+      const length = values[0].size();
+      values.forEach((value) => {
+        if ((value as Vec).size() !== length) {
+          throw new Error('All matrix vectors must have the same size')
+        }
+      })
+      this._matrix = values as Vec[];
+    }
+    // basic number/string/bignumber values
+    else {
+      this._matrix = values.map((value) => new Vec(value));
+    }
+  }
+
+  static empty(width: number, height: number, defaultValue=0) {
+    return new Matrix(Array(height).map(() => Vec.empty(width, defaultValue)));
+  }
+
+  transpose(): Matrix {
+    let values: Vec[] = []
+    const elements = this._matrix.length > 0
+      ? this._matrix[0].size()
+      : 0;
+    for (let column = 0; column < elements; column ++) {
+      let val: BigNumber[] = [];
+      for (let row = 0; row < this._matrix.length; row ++) {
+        val.push(this._matrix[row].get(column));
+      }
+      values.push(new Vec(val));
+    }
+    return new Matrix(values);
+  }
+
+  // Element wise operations
+  add(matrix: number|string|BigNumber|Vec|Matrix): Matrix {
+    return this._action(matrix, (v1, v2) => v1.add(v2));
+  }
+  sub(matrix: number|string|BigNumber|Vec|Matrix): Matrix { 
+    return this._action(matrix, (v1, v2) => v1.sub(v2));
+  }
+  div(matrix: number|string|BigNumber|Vec|Matrix): Matrix {
+    return this._action(matrix, (v1, v2) => v1.div(v2));
+  }
+  mul(matrix: number|string|BigNumber|Vec|Matrix): Matrix {
+    return this._action(matrix, (v1, v2) => v1.mul(v2));
+  }
+
+  // 
+  dot(value: Vec|Matrix): Matrix {
+    const val = value instanceof Matrix 
+      ? value.transpose() 
+      : new Matrix(value);
+
+    const vals = this._matrix.map((v1) => 
+      new Vec(val._matrix.map((v2) => v1.mul(v2).sum()))
+    )
+    return new Matrix(vals);
+  }
+
+  get(index: number): Vec {
+    if (index < 0 || index > this._matrix.length) {
+      throw new Error('Out of bounds Matrix:get');
+    } 
+    return this._matrix[index];
+  }  
+  toString(): string {
+    return `[${this._matrix.map((vec) => vec.toString()).join(', \n ')}]`;
+  }
+  toNumber(): number[][] {
+    return this._matrix.map((vec) => vec.toNumber());
+  }
+  size(): [number, number] {
+    const len = this._matrix.length;
+    return [len, len > 0 ? this._matrix[0].size() : 0];
+  }
+
+
+  // Private functions
+  _action(value: number|string|BigNumber|Vec|Matrix, fn: (v1: Vec, v2: BigNumber|Vec) => Vec): Matrix {
+    let values: Vec[] = [];
+    if (value instanceof Matrix) {
+      const [ch] = this.size();
+      const [vh] = value.size();
+      if (ch !== vh) {
+        throw new Error('Matrix sizes do not match!')
+      }
+      values = this._matrix.map((vec, index) => fn(vec, value.get(index)));
+    } else if (value instanceof Vec || value instanceof BigNumber) {
+      values = this._matrix.map((vec) => fn(vec, value));
+    } else {
+      const val = new BigNumber(value);
+      values = this._matrix.map((vec) => fn(vec, val));
+    }
+    return new Matrix(values);
+  }
+}
---
+++ src/vector.ts
@@ -0,0 +1,85 @@
+import BigNumber from "bignumber.js";
+
+export type VecBasicValues =
+  | string[]
+  | number[]
+  | BigNumber[];
+
+export default class Vec {
+    _vector: BigNumber[];
+    constructor(values?: VecBasicValues | Vec) {
+      if (!values) { 
+        this._vector = [];
+      } else if (values instanceof Vec) {
+        this._vector = [...values._vector];
+      } else if (values.length > 0 && values[0] instanceof BigNumber) {
+        this._vector = values as BigNumber[];
+      } else {
+        this._vector = values.map((value) => new BigNumber(value))
+      }
+    }
+  
+
+  static empty(size: number, defaultValue=0): Vec {
+    return new Vec(Array(size).fill(defaultValue))
+  }
+
+  // Math
+  mul(vec: string|number|BigNumber|Vec): Vec {
+    return this._action(vec, (v1, v2) => v1.multipliedBy(v2))
+  }
+  div(vec: string|number|BigNumber|Vec): Vec {
+    return this._action(vec, (v1, v2) => v1.div(v2))
+  }
+  add(vec: string|number|BigNumber|Vec): Vec {
+    return this._action(vec, (v1, v2) => v1.plus(v2))
+  }
+  sub(vec: string|number|BigNumber|Vec): Vec {
+    return this._action(vec, (v1, v2) => v1.minus(v2))
+  }
+  sum(): BigNumber {
+    return this._vector.reduce((prev, curr) => prev.plus(curr), new BigNumber(0))
+  }
+
+  // 
+  toNumber(): number[] {
+    return this._vector.map((value) => value.toNumber())
+  }
+  toString(): string {
+    return `[${this._vector.map((value) => value.toString()).join(', ')}]`;
+  }
+  copy(): Vec {
+    return new Vec(this._vector);;
+  }
+  size(): number {
+    return this._vector.length;
+  }
+  get(index: number): BigNumber {
+    if (index > this.size() || index < 0) {
+      throw new Error('Pointer is fucked');
+    }
+    return this._vector[index];
+  }
+
+  // Private functions
+  _action(vec: string|number|BigNumber|Vec, fn: (value1: BigNumber, vec2: BigNumber) => BigNumber): Vec {
+    const get = (index: number): BigNumber => {
+      if (vec instanceof Vec) {
+        if (vec.size() !== 1 && vec.size() !== this.size()) {
+          throw new Error('Vectors are not in same size!');
+        }
+        return vec.size() === 1 
+          ? vec.get(0) 
+          : vec.get(index);
+      } else {
+        return new BigNumber(vec);
+      }
+    }
+
+    return new Vec(this._vector.map((value, index) => fn(value, get(index))));
+  }
+}
+
+const emptyIntVec = (size: number, defaultValue: number = 0) => {
+
+};
